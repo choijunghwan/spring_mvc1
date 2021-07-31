5cc1eaf4716c5d5801496badc5b22b0f0b958ecd
ChoiJungHwan <vkdlxj3562@naver.com>
2021-08-01 00:37:25 +0900

    프론트 컨트롤러 v3

--- src/main/java/hello/servlet/web/frontcontroller/v3/controller/MemberListControllerV3.java
+++ src/main/java/hello/servlet/web/frontcontroller/v3/controller/MemberListControllerV3.java
@@ -21,7 +21,7 @@ public class MemberListControllerV3 implements ControllerV3 {
     public ModelView process(Map<String, String> paramMap) {
         List<Member> members = memberRepository.findAll();
 
-        ModelView mv = new ModelView("/");
+        ModelView mv = new ModelView("members");
         mv.getModel().put("members", members);
         return mv;
     }
